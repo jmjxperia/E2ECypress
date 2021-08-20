6fba87af84dca1889bdef11b6fa2f2aaa89885a7
jmjxperia <jathinjater1996@gmail.com>
2021-08-20 16:29:08 +0530

    load data from fixture

--- cypress/integration/e2e/HomePage.js
+++ cypress/integration/e2e/HomePage.js
@@ -1,18 +1,23 @@
 /// <reference types="cypress" />
 import LandingPage from '../../support/pageObjects/LandingPage'
+
 describe('Home page test ',function(){
 
     before(function(){
 
         cy.visit('http://advantageonlineshopping.com/#/')
-
+        cy.fixture('loadData').then(function(data){
+            this.data=data
+        
+        })
     })
+    
     it('check login',function(){
         const landing=new LandingPage()
         cy.title().should('include','Advantage Shopping')
         landing.clickLoginHeader().click()
-        landing.enterUsername().type("testuser1")
-        landing.enterPassword().type("qwertY1")
+        landing.enterUsername().type(this.data.username)
+        landing.enterPassword().type(this.data.password)
         landing.clickSignIn().click()
     })
 })
